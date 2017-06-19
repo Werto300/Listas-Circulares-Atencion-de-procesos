8183e5a2ff16dac6c0b0d5069151901d61883570
José Luis Fajardo Borjas <werto300@gmail.com>
2017-06-19 16:20:20 -0500

    Add files via upload

---
+++ Listas circulares Atencion de procesos/Listas circulares Atencion de procesos/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Listas_circulares_Atencion_de_procesos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEmpezar = new System.Windows.Forms.Button();
+            this.txtLista = new System.Windows.Forms.TextBox();
+            this.txtResultados = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEmpezar
+            // 
+            this.btnEmpezar.Location = new System.Drawing.Point(12, 12);
+            this.btnEmpezar.Name = "btnEmpezar";
+            this.btnEmpezar.Size = new System.Drawing.Size(75, 23);
+            this.btnEmpezar.TabIndex = 0;
+            this.btnEmpezar.Text = "Empezar";
+            this.btnEmpezar.UseVisualStyleBackColor = true;
+            this.btnEmpezar.Click += new System.EventHandler(this.btnEmpezar_Click);
+            // 
+            // txtLista
+            // 
+            this.txtLista.Location = new System.Drawing.Point(12, 58);
+            this.txtLista.Multiline = true;
+            this.txtLista.Name = "txtLista";
+            this.txtLista.Size = new System.Drawing.Size(168, 240);
+            this.txtLista.TabIndex = 1;
+            // 
+            // txtResultados
+            // 
+            this.txtResultados.Location = new System.Drawing.Point(197, 58);
+            this.txtResultados.Multiline = true;
+            this.txtResultados.Name = "txtResultados";
+            this.txtResultados.Size = new System.Drawing.Size(231, 105);
+            this.txtResultados.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 310);
+            this.Controls.Add(this.txtResultados);
+            this.Controls.Add(this.txtLista);
+            this.Controls.Add(this.btnEmpezar);
+            this.Name = "Form1";
+            this.Text = "Atención de procesos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEmpezar;
+        private System.Windows.Forms.TextBox txtLista;
+        private System.Windows.Forms.TextBox txtResultados;
+    }
+}
+
